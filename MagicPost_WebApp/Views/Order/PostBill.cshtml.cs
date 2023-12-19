4d77190384361ff49953d1d4ae2d27578a933d8f
hoanghaoes <hoanghaoes@gmail.com>
2023-12-20 02:13:58 +0700

    update bill of delivery in html

---
+++ MagicPost_WebApp/Views/Order/PostBill.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MagicPost_WebApp.Views.Order
+{
+    public class PostBillModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
