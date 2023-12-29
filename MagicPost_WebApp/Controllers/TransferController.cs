0772f2bd427d665eeb8c8b6355dde39a03326739
Playmaker0210 <tenbin2003@gmail.com>
2023-12-29 18:37:57 +0700

    done for transfer

--- MagicPost_WebApp/Controllers/TransferController.cs
+++ MagicPost_WebApp/Controllers/TransferController.cs
@@ -5,6 +5,9 @@ using MagicPost_ViewModel.Common;
 using MagicPost_ViewModel.Orders;
 using MagicPost_ViewModel.System.Users;
 using Microsoft.AspNetCore.Mvc;
+using MagicPost__Data.EF;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 
 namespace MagicPost_WebApp.Controllers
 {
@@ -12,18 +15,39 @@ namespace MagicPost_WebApp.Controllers
         public class TransferController : Controller
         {
             private readonly ITransferApiClient _orderApiClient;
+            private readonly MagicPostDbContext _context;
           
-            public TransferController(ITransferApiClient orderApiClient)
+            public TransferController(ITransferApiClient orderApiClient, MagicPostDbContext context)
             {
                 _orderApiClient = orderApiClient;
+                _context = context;
             }
 
             [HttpGet]
             public async Task<IActionResult> Create(int id)
             {
+                ViewBag.ListTK = null;
+                ViewBag.ListGD = null;
                 var temp = await GetTransferRequest(id);
-             ViewBag.orderId = id;
-            return View(temp);
+                ViewBag.orderId = id;
+                var ord = await _context.Orders.FindAsync(id);
+                ViewBag.Status = ord.Status;
+                var tempId = ord.DiemTapKetId;
+                if (tempId != null)
+                {
+                var diemTapKetAddresses = _context.DiemTapKets
+                    .Where(d => d.Id != tempId)
+                    .Select(d => d.Address)
+                    .ToList();
+                ViewBag.ListTK = diemTapKetAddresses;
+
+                var diemGiaoDichAddresses = _context.DiemGiaoDichs
+                    .Where(d => d.DiemTapKetId == tempId)
+                    .Select(d => d.Address)
+                    .ToList();
+                ViewBag.ListGD = diemGiaoDichAddresses;
+                }
+                return View(temp);
             }
 
             [HttpPost]
@@ -34,11 +58,23 @@ namespace MagicPost_WebApp.Controllers
                 if (!ModelState.IsValid)
                     return View(request);
                 
-                var result = await _orderApiClient.CreateTransfer(request.OrderId, request);
+                if(ViewBag.ListGD != null)
+                {
+                var diemGiaoDich = await _context.DiemGiaoDichs.FindAsync(request.ToGD);
+                request.ToDiemGd = diemGiaoDich.Id;
+                    
+                }
+
+                if (ViewBag.ListTK != null)
+                {
+                var diemTapKet = await _context.DiemTapKets.FindAsync(request.ToTk);
+                request.ToDiemTk = diemTapKet.Id;
+
+                }
+            var result = await _orderApiClient.CreateTransfer(request.OrderId, request);
                 if (result)
                 {
-                    TempData["result"] = "Thêm mới sản phẩm thành công";
-
+                    TempData["result"] = "Chuyển sản phẩm thành công";
                     return RedirectToAction("About", "Home");
 
                 }
